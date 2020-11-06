a00a0c557c1e27a555e0c76eef1abfc85e4439af
Puglisi <39974597+npuglisi@users.noreply.github.com>
2020-11-06 02:54:32 -0300

    correção df

--- zap/reading/read.py
+++ zap/reading/read.py
@@ -7,10 +7,8 @@ path = ".\\dataset"
 
 def read_json():
     df = spark.read.json(path+"\\part-00000.json", multiLine = "false")
-    """
     df = df.union(spark.read.json(path+"\\part-00001.json", multiLine = "false"))
     df = df.union(spark.read.json(path+"\\part-00002.json", multiLine = "false"))
     df = df.union(spark.read.json(path+"\\part-00003.json", multiLine = "false"))
     df = df.union(spark.read.json(path+"\\part-00004.json", multiLine = "false"))
-    """
     return df
